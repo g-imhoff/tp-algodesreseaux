4a490eae734f6497c54c77e049a2b33bfae23b86
gimhoff <gimhoff@osr-etudiant-1.srv.unistra.fr>
2024-09-20 15:16:39 +0200

    il ne manque plus que le valgrind

--- 1-udp/1.2-recepteur/receiver-udp.c
+++ 1-udp/1.2-recepteur/receiver-udp.c
@@ -33,7 +33,7 @@ noreturn void usage(const char *msg) {
 struct addrinfo *config(const char *port) {
   struct addrinfo hints;
   hints.ai_flags = AI_PASSIVE;
-  hints.ai_family = AF_UNSPEC;
+  hints.ai_family = AF_INET6;
   hints.ai_socktype = SOCK_DGRAM;
   hints.ai_protocol = IPPROTO_UDP;
 
@@ -48,7 +48,7 @@ struct addrinfo *config(const char *port) {
 int create_socket(const struct addrinfo *host) {
   int fdsock = socket(host->ai_family, host->ai_socktype, host->ai_protocol);
   CHK(fdsock);
-
+  
   int value = 0;
   CHK(setsockopt(fdsock, IPPROTO_IPV6, IPV6_V6ONLY, &value, sizeof value));
 
@@ -57,22 +57,19 @@ int create_socket(const struct addrinfo *host) {
 
 void receive_and_print(const int fdsock) {
   char buffer[BUFFERLEN];
-  struct sockaddr_storage from; // To handle both IPv4 and IPv6 addresses
+  struct sockaddr_storage from; 
   socklen_t fromlen = sizeof(from);
 
-  // Receive data from the client
   ssize_t size = recvfrom(fdsock, buffer, BUFFERLEN - 1, 0,
                           (struct sockaddr *)&from, &fromlen);
   CHK(size);
-  buffer[size] = '\0'; // Null-terminate the received message
+  buffer[size] = '\0'; 
 
-  // Get the sender's address and port
   char host[NI_MAXHOST], serv[NI_MAXSERV];
   int flags = NI_NUMERICHOST | NI_NUMERICSERV;
   CHKA(getnameinfo((struct sockaddr *)&from, fromlen, host, sizeof(host), serv,
                    sizeof(serv), flags));
 
-  // Print the sender's IP, port, and the message
   printf("%s %s a envoyé : %s\n", host, serv, buffer);
 }
 
